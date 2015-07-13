90775614b087fcef671fd3788db981ea21e32b54
nepeat <nepeat@users.noreply.github.com>
2015-07-13 02:03:37 -0800

    Get session cookie from the server instead of doing it client side

--- erigam/static/js/chat.js
+++ erigam/static/js/chat.js
@@ -21,17 +21,6 @@ function linkify(inputText) {
     return replacedText;
 }
 
-function readCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0;i < ca.length;i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1,c.length);
-        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length,c.length);
-    }
-    return null;
-}
-
 $(document).ready(function() {
 
     var SEARCH_PERIOD = 1;
@@ -832,10 +821,10 @@ $(document).ready(function() {
                     return false;
                 }
 
-                if (jQuery.trim($('#textInput').val())=='/usr') {
-                    session = readCookie('session');
-                    var msg = {counter:"-2",color:"000000",line:"[SYSTEM] Your cookie: " + session};
-                    addLine(msg);
+                if (jQuery.trim($('#textInput').val()) == '/usr') {
+                    $.post("/chat_ajax/getSession", function(session) {
+                        addLine({counter:"-2",color:"000000",line:"[SYSTEM] Your cookie: " + session});
+                    });
                     $('#textInput').val('');
                 } else if ($('#textInput').val() !== '') {
                     if (pingInterval) {

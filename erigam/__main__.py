c397999626e9738c2a5f43109480bfe942a2680b
nepeat <nepeat@users.noreply.github.com>
2015-07-12 20:37:49 -0800

    Mixed up the purposes of __main__ and __init__

--- erigam/__main__.py
+++ erigam/__main__.py
@@ -1,47 +1,4 @@
-from flask import Flask
+from erigam import app
 
-from erigam.lib.request_methods import (
-    populate_all_chars,
-    connect_db,
-    create_session,
-    set_cookie,
-    disconnect_db
-)
-
-from erigam.views import (
-    main,
-    backend,
-    chat,
-    admin,
-    log
-)
-
-app = Flask(__name__)
-
-# Pre and post request stuff
-app.before_first_request(populate_all_chars)
-app.before_request(connect_db)
-app.before_request(create_session)
-app.after_request(set_cookie)
-app.teardown_request(disconnect_db)
-
-# Flask settings
-app.url_map.strict_slashes = False
-
-# Jinja settings
-app.jinja_env.trim_blocks = True
-app.jinja_env.lstrip_blocks = True
-
-# Throw tracebacks to console
-app.config['PROPAGATE_EXCEPTIONS'] = True
-
-# Register Blueprints
-app.register_blueprint(main.blueprint)
-app.register_blueprint(backend.blueprint, url_prefix='/chat_ajax')
-app.register_blueprint(chat.blueprint, url_prefix='/chat')
-app.register_blueprint(admin.blueprint, url_prefix='/admin')
-app.register_blueprint(log.blueprint, url_prefix='/log')
-
-# Debug
 if __name__ == "__main__":
     app.run(port=5000, host="0.0.0.0", debug=True, threaded=True)

0af1613b9f06ef5322e2b0368d686d00f574773c
nepeat <nepeat@users.noreply.github.com>
2015-07-14 01:08:15 -0800

    RxP.TC to RP.TC

--- erigam/static/js/chat.js
+++ erigam/static/js/chat.js
@@ -16,7 +16,7 @@ $(document).ready(function() {
 	var MOD_GROUPS = ['globalmod', 'mod', 'mod2', 'mod3'];
 	var GROUP_RANKS = { 'globalmod': 6, 'mod': 5, 'mod2': 4, 'mod3': 3, 'user': 2, 'silent': 1 };
 	var GROUP_DESCRIPTIONS = {
-		'globalmod': { title: 'God tier moderator', description: 'RxP.TC staff.', shorthand: 'Staff' },
+		'globalmod': { title: 'God tier moderator', description: 'RP.TC staff.', shorthand: 'Staff' },
 		'mod': { title: 'Professional Wet Blanket', description: 'can silence, kick and ban other users.', shorthand: 'Wet Blanket' },
 		'mod2': { title: 'Bum\'s Rusher', description: 'can silence and kick other users.', shorthand: 'Bum Rusher' },
 		'mod3': { title: 'Amateur Gavel-Slinger', description: 'can silence other users.', shorthand: 'Gavel Slinger' },

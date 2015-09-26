0d83a2b8bde62e2701c7f6d49d14764bcc1f6dbe
nepeat <nepeat@users.noreply.github.com>
2015-09-26 00:54:11 -0800

    Matchmaker use zip instead of range hacks to iterate by twos.

--- erigam/matchmaker.py
+++ erigam/matchmaker.py
@@ -2,11 +2,18 @@
 from redis import Redis
 import uuid
 import time
+import logging
+import os
 
 from erigam.lib.api import chatapi
 from erigam.lib.model import sm, Message
 from erigam.lib.messages import send_message
 from erigam.lib.request_methods import redis_pool
+
+logger = logging.getLogger()
+
+if 'DEBUG' in os.environ:
+    logger.setLevel("DEBUG")
 
 OPTION_LABELS = {
     'para0': u'script style',
@@ -42,45 +49,44 @@ if __name__ == '__main__':
         searchers = redis.zrange('searchers', 0, -1)
 
         if len(searchers) >= 2:  # if there aren't at least 2 people, there can't be matches
-
-            all_chars = redis.smembers('all-chars')
             sessions = [{
                 'id': session_id,
                 'options': redis.hgetall('session.'+session_id+'.picky-options'),
             } for session_id in searchers]
 
-            already_matched = set()
-            for n in range(len(sessions)):
-                for m in range(n+1, len(sessions)):
-                    print sessions[n]['id'], sessions[m]['id']
-                    if (
-                        sessions[n]['id'] not in already_matched
-                        and sessions[m]['id'] not in already_matched
-                    ):
-                        compatible, selected_options = check_compatibility(sessions[n], sessions[m])
-                        if not compatible:
-                            print compatible, selected_options
-                            continue
-                        chat = str(uuid.uuid4()).replace('-', '')
-                        print 'Match found, sending to %s.' % chat
-                        print '%s: options %s' % (sessions[n]['id'], sessions[n]['options'])
-                        print '%s: options %s' % (sessions[m]['id'], sessions[m]['options'])
-                        log = chatapi.create_chat(db, redis, chat, 'saved')
-                        if len(selected_options) > 0:
-                            option_text = u', '.join(OPTION_LABELS[_] for _ in selected_options)
-                            send_message(db, redis, Message(
-                                log_id=log.id,
-                                type=u"message",
-                                counter=-1,
-                                text=u"This is a {option} chat.".format(option=option_text)
-                            ))
-                        redis.hset('session.'+sessions[n]['id']+'.match', 'chat', chat)
-                        redis.hset('session.'+sessions[m]['id']+'.match', 'chat', chat)
-                        redis.hset('session.'+sessions[n]['id']+'.match', 'log', log.id)
-                        redis.hset('session.'+sessions[m]['id']+'.match', 'log', log.id)
-                        redis.zrem('searchers', sessions[n]['id'])
-                        redis.zrem('searchers', sessions[m]['id'])
-                        already_matched.add(sessions[n]['id'])
-                        already_matched.add(sessions[m]['id'])
+            # https://docs.python.org/2/library/functions.html#zip
+            for searcher1, searcher2 in zip(*[iter(sessions)]*2):
+                logging.debug("Matching %s and %s" % (searcher1['id'], searcher2['id']))
+                compatible, selected_options = check_compatibility(searcher1, searcher2)
+
+                if not compatible:
+                    logging.debug("%s not compatible with %s" % (searcher1['id'], searcher2['id']))
+                    continue
+
+                chat = str(uuid.uuid4()).replace('-', '')
+                logging.debug('Match found, sending to %s.' % chat)
+                logging.debug('%s: options %s' % (searcher1['id'], searcher1['options']))
+                logging.debug('%s: options %s' % (searcher2['id'], searcher2['options']))
+                log = chatapi.create_chat(db, redis, chat, 'saved')
+
+                # Send options message if options are present.
+                if len(selected_options) > 0:
+                    option_text = u', '.join(OPTION_LABELS[_] for _ in selected_options)
+                    send_message(db, redis, Message(
+                        log_id=log.id,
+                        type=u"message",
+                        counter=-1,
+                        text=u"This is a {option} chat.".format(option=option_text)
+                    ))
+
+                matchdata = {
+                    "chat": chat,
+                    "log": log.id
+                }
+
+                redis.hmset('session.'+searcher1['id']+'.match', matchdata)
+                redis.hmset('session.'+searcher2['id']+'.match', matchdata)
+                redis.zrem('searchers', searcher1['id'])
+                redis.zrem('searchers', searcher2['id'])
 
         time.sleep(1)

859f0ad5596c521eb419a1390b5196b885ce2a71
nepeat <nepeat@users.noreply.github.com>
2015-09-27 14:17:03 -0800

    Fix code injection by making JSON clean.

--- erigam/views/chat.py
+++ erigam/views/chat.py
@@ -87,7 +87,7 @@ def chat(chat_url=None):
         chat_meta=chat_meta,
         messages=messages,
         character_dict=g.user.json_info(),
-        user_info=json.dumps(user_info),
+        user_info=user_info,
         legacy_bbcode=g.redis.sismember('use-legacy-bbcode', chat_url)
     )
 

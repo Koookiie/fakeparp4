1aac0b2475f00154b4318b40c3ec923f7a8fc39a
nepeat <nepeat@users.noreply.github.com>
2015-10-01 15:22:27 -0800

    Log pages are safe so use the raw bbcode encode.

--- erigam/static/js/erigam/views/log.js
+++ erigam/static/js/erigam/views/log.js
@@ -8,7 +8,7 @@ define("erigam/views/log", ['jquery', 'erigam/bbcode'], function($, bbcode) {
 			$('#archiveConversation p').each(function() {
 				var line;
 				if (bbcodeon == 1) {
-					line = bbcode.encode($(this).html());
+					line = bbcode.raw_encode($(this).html());
 					$(this).html(line);
 				} else {
 					line = bbcode.remove($(this).html());

df8e416236861f02994d89a602fd4c754220e47a
Hex <hecksadecimal@outlook.com>
2021-07-07 16:11:48 -0400

    Fix preferences maybe

--- erigam/lib/api/chat.py
+++ erigam/lib/api/chat.py
@@ -6,6 +6,9 @@ from erigam.lib.model import Log, Chat, ChatSession
 from sqlalchemy.orm.exc import NoResultFound
 
 def create(sql, redis, url, chattype="group"):
+    if redis.hexists('punish-scene', g.user.ip):
+        raise ValueError('pandamode')
+
     if sql.query(Log).filter(Log.url == url).scalar():
         raise ValueError('chaturl_taken')
 
